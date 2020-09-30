47afd017a28dd837c6f801912425f4faf6f4ffb6
Gabriele Gianoglio <gabriele.gianoglio@consulenti.csi.it>
2020-09-30 15:18:48 +0200

    DDL Postgres

---
+++ dbscript/unppreferences.sql
@@ -0,0 +1,98 @@
+CREATE TABLE unppreferences.broadcast (
+	uuid varchar(36) NOT NULL,
+	name varchar(255) NULL,
+	service varchar(255) NULL,
+	scheduled_at timestamptz NULL,
+	created_at timestamptz NULL DEFAULT now(),
+	sent_at timestamptz NULL,
+	status varchar(255) NOT NULL,
+	mex text NULL,
+	"token" text NULL,
+	CONSTRAINT idx_10515459_primary PRIMARY KEY (uuid)
+);
+
+
+CREATE TABLE unppreferences.services (
+	uuid varchar(36) NOT NULL,
+	name varchar(255) NOT NULL,
+	channels varchar(255) NULL,
+	tags_csv text NULL,
+	tags _text NULL,
+	CONSTRAINT idx_10515524_primary PRIMARY KEY (uuid)
+);
+CREATE UNIQUE INDEX idx_10515524_name ON unppreferences.services USING btree (name);
+CREATE INDEX idx_10515524_tags_ft_index ON unppreferences.services USING gin (to_tsvector('simple'::regconfig, tags_csv));
+CREATE INDEX services_tags_array_idx_gin ON unppreferences.services USING gin (tags);
+
+
+CREATE TABLE unppreferences.users (
+	user_id varchar(255) NOT NULL,
+	sms varchar(30) NULL,
+	phone varchar(30) NULL,
+	email varchar(255) NULL,
+	push text NULL,
+	"language" varchar(255) NULL,
+	interests varchar(255) NULL,
+	CONSTRAINT users_pk PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE unppreferences.users2 (
+	user_id varchar(255) NOT NULL,
+	sms varchar(30) NULL,
+	phone varchar(30) NULL,
+	email varchar(255) NULL,
+	push text NULL,
+	"language" varchar(255) NULL,
+	interests varchar(255) NULL
+);
+
+
+CREATE TABLE unppreferences.users_s (
+	user_id varchar(255) NOT NULL,
+	sms varchar(255) NULL,
+	phone varchar(255) NULL,
+	email varchar(255) NULL,
+	push text NULL,
+	"language" varchar(255) NULL,
+	interests varchar(255) NULL,
+	"timestamp" timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT users_s_pk PRIMARY KEY (user_id, "timestamp")
+);
+
+
+CREATE TABLE unppreferences.users_services (
+	uuid varchar(36) NOT NULL,
+	user_id varchar(255) NOT NULL,
+	service_name varchar(255) NOT NULL,
+	channels varchar(255) NULL,
+	CONSTRAINT idx_10515737_primary PRIMARY KEY (uuid)
+);
+CREATE UNIQUE INDEX idx_10515737_user_id ON unppreferences.users_services USING btree (user_id, service_name);
+
+
+CREATE TABLE unppreferences.users_services_s (
+	uuid varchar(36) NOT NULL,
+	user_id varchar(255) NOT NULL,
+	service_name varchar(255) NOT NULL,
+	channels varchar(255) NULL,
+	"timestamp" timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT idx_10515801_primary PRIMARY KEY (uuid, "timestamp")
+);
+
+
+CREATE TABLE unppreferences.users_terms (
+	user_id varchar(255) NOT NULL,
+	accepted_at timestamptz NULL,
+	hashed_terms varchar(255) NULL,
+	CONSTRAINT idx_10515853_primary PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE unppreferences.users_terms_s (
+	user_id varchar(255) NOT NULL,
+	accepted_at timestamptz NULL,
+	hashed_terms varchar(255) NULL,
+	"timestamp" timestamptz NOT NULL,
+	CONSTRAINT idx_10515908_primary PRIMARY KEY (user_id, "timestamp")
+);
